c8fac5f9921b94c59ed19393d947bf96a37f3030
Andrius Mudinas <keistokas@gmail.com>
2018-02-26 22:05:27 +0000

    Initial check in

---
+++ src/Wikiled.Market/Analysis/AnalysisManager.cs
@@ -0,0 +1,11 @@
+﻿using Trady.Core.Infrastructure;
+
+namespace Wikiled.Market.Analysis
+{
+    public class AnalysisManager
+    {
+        public void Start(IImporter importer)
+        {
+        }
+    }
+}
---
+++ src/Wikiled.Market/Analysis/Credentials.cs
@@ -0,0 +1,7 @@
+﻿namespace Wikiled.Market.Analysis
+{
+    public static class Credentials
+    {
+        public const string QuandlKey = "YOUR_API_KEY";
+    }
+}
