159ecc3047381c77e4998e4e2d2c09390039f2d6
Andrius <andrius@wikiled.com>
2018-02-27 11:42:54 +0000

    Update

--- src/Wikiled.Market/Analysis/Credentials.cs
+++ src/Wikiled.Market/Analysis/Credentials.cs
@@ -4,6 +4,10 @@ namespace Wikiled.Market.Analysis
 {
     public static class Credentials
     {
+        // git update-index --assume-unchanged Credentials.cs
+        // git update-index --no-assume-unchanged Credentials.cs
+        // git ls-files -v|grep '^h'
+
         public static string QuandlKey => Environment.GetEnvironmentVariable("QuandlKey") ?? "YOUR_API_KEY";
     }
 }
