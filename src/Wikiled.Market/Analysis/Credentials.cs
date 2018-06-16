335a5ed65646a2bc153a2adb5021c9718a85283c
Andrius Mudinas <keistokas@gmail.com>
2018-06-16 18:59:24 +0100

    Change variable naming

--- src/Wikiled.Market/Analysis/Credentials.cs
+++ src/Wikiled.Market/Analysis/Credentials.cs
@@ -6,15 +6,15 @@ namespace Wikiled.Market.Analysis
 {
     public static class Credentials
     {
-        public static string QuandlKey { get; } = Environment.GetEnvironmentVariable("QuandlKey")?.Trim();
+        public static string QuandlKey { get; } = Environment.GetEnvironmentVariable("QUANDL_KEY")?.Trim();
 
         public static ITwitterCredentials TwitterAppCredentials { get; } =
-            Auth.SetApplicationOnlyCredentials(Environment.GetEnvironmentVariable("AppKey")?.Trim(), Environment.GetEnvironmentVariable("AppSecret")?.Trim());
+            Auth.SetApplicationOnlyCredentials(Environment.GetEnvironmentVariable("TW_APP_KEY")?.Trim(), Environment.GetEnvironmentVariable("TW_APP_SECRET")?.Trim());
 
         public static ITwitterCredentials TwitterCredentials { get; } = new TwitterCredentials(
-            Environment.GetEnvironmentVariable("CONSUMER_KEY")?.Trim(),
-            Environment.GetEnvironmentVariable("CONSUMER_SECRET")?.Trim(),
-            Environment.GetEnvironmentVariable("APP_KEY")?.Trim(),
-            Environment.GetEnvironmentVariable("APP_SECRET")?.Trim());
+            Environment.GetEnvironmentVariable("TW_CONSUMER_KEY")?.Trim(),
+            Environment.GetEnvironmentVariable("TW_CONSUMER_SECRET")?.Trim(),
+            Environment.GetEnvironmentVariable("TW_APP_KEY")?.Trim(),
+            Environment.GetEnvironmentVariable("TW_APP_SECRET")?.Trim());
     }
 }
