1a30f88ce8a76955a62dc6d086fdf83c5ddad63e
Andrius Mudinas <andrius@wikiled.com>
2018-06-19 12:06:51 +0100

    Update monitoring

--- src/Wikiled.Market.Console/Commands/TwitterBotCommand.cs
+++ src/Wikiled.Market.Console/Commands/TwitterBotCommand.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
 using System.Reactive.Linq;
 using System.Text;
 using System.Threading;
@@ -9,10 +11,12 @@ using Trady.Importer;
 using Tweetinvi;
 using Tweetinvi.Models;
 using Tweetinvi.Parameters;
+using Wikiled.Common.Net.Client;
 using Wikiled.Common.Utilities.Config;
 using Wikiled.Common.Utilities.Rx;
 using Wikiled.Console.Arguments;
 using Wikiled.Market.Analysis;
+using Wikiled.Market.Sentiment;
 using Wikiled.Twitter.Security;
 
 namespace Wikiled.Market.Console.Commands
@@ -30,6 +34,12 @@ namespace Wikiled.Market.Console.Commands
 
         private IDisposable timer = default;
 
+        private IDisposable twitterTimer = null;
+
+        private StreamMonitor monitor;
+
+        private IStockTracker[] trackers;
+
         public override string Name => "Bot";
 
         [Description("For what stocks generate prediction")]
@@ -39,7 +49,9 @@ namespace Wikiled.Market.Console.Commands
 
         public override Task StopExecution(CancellationToken token)
         {
+            monitor?.Dispose();
             timer?.Dispose();
+            twitterTimer?.Dispose();
             return base.StopExecution(token);
         }
 
@@ -74,7 +86,10 @@ namespace Wikiled.Market.Console.Commands
                 throw new ArgumentNullException("QuandlKey not found");
             }
 
+            trackers = Stocks.Split(',').Select(item => new StockTracker(configuration, item)).ToArray();
             Process();
+            monitor = new StreamMonitor(new BasicAuthentication(cred), new SentimentAnalysis(new StreamApiClient(new HttpClient(), new Uri("http://sentiment.wikiled.com/api/sentiment/"))), trackers);
+            monitor.Start(@"c:\logging\market\twitter");
             return Task.CompletedTask;
         }
 
@@ -83,21 +98,34 @@ namespace Wikiled.Market.Console.Commands
             var instance = new AnalysisManager(new DataSource(new QuandlWikiImporter(Analysis.Credentials.QuandlKey)), new ClassifierFactory());
             var timerCreator = new ObservableTimer(configuration);
             timer = timerCreator.Daily(TimeSpan.FromHours(6)).Subscribe(item => ProcessMarket(instance));
+            twitterTimer = Observable.Interval(TimeSpan.FromHours(3)).Subscribe(item => ProcessSentiment());
+        }
+
+        private void ProcessSentiment()
+        {
+            log.Info("Processing market");
+            StringBuilder text = new StringBuilder();
+            foreach (var stock in trackers)
+            {
+                text.AppendLine($"{stock.Twitter} ({stock.TotalMessages}) with average sentiment - {stock.AverageSentiment()}(24H) {stock.AverageSentiment(1)}(1H)");
+            }
+
+            PublishMessage(text.ToString());
         }
 
         private void ProcessMarket(AnalysisManager instance)
         {
-            var stocks = Stocks.Split(',');
             log.Info("Processing market");
-            foreach (var stock in stocks)
+            foreach (var stock in trackers)
             {
                 log.Info("Processing {0}", stock);
                 StringBuilder text = new StringBuilder();
-                var result = instance.Start(stock).Result;
+                var result = instance.Start(stock.Stock).Result;
                 var sellAccuracy = result.Performance.PerClassMatrices[0].Accuracy;
                 var buyAccuracy = result.Performance.PerClassMatrices[1].Accuracy;
                 text.AppendLine($"${stock} trading signals ({sellAccuracy * 100:F0}%/{buyAccuracy * 100:F0}%)");
-                for (int i = 0; i < result.Predictions.Length; i++)
+                text.AppendLine($"Total twitter messages {stock.TotalMessages} with average sentiment - {stock.AverageSentiment()}(24H) {stock.AverageSentiment(1)}(1H)");
+                for (int i = 0; i < result.Predictions.Length || i < 2; i++)
                 {
                     var prediction = result.Predictions[result.Predictions.Length - i - 1];
                     log.Info("{2}, Predicted T-{0}: {1}\r\n", i, prediction, stock);
--- src/Wikiled.Market/Sentiment/StreamMonitor.cs
+++ src/Wikiled.Market/Sentiment/StreamMonitor.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using NLog;
 using Tweetinvi.Models.DTO;
+using Wikiled.Common.Extensions;
+using Wikiled.Text.Analysis.Twitter;
 using Wikiled.Twitter.Persistency;
 using Wikiled.Twitter.Security;
 using Wikiled.Twitter.Streams;
 
 namespace Wikiled.Market.Sentiment
 {
-    public class StreamMonitor : IDisposable, IPersistency
+    public class StreamMonitor : IDisposable
     {
+        private static readonly Logger log = LogManager.GetCurrentClassLogger();
+
         private TimingStreamSource streamSource;
 
         private MonitoringStream stream;
@@ -19,29 +28,66 @@ namespace Wikiled.Market.Sentiment
 
         private readonly ISentimentAnalysis sentiment;
 
-        public StreamMonitor(IAuthentication authentication, ISentimentAnalysis sentiment)
+        private IDisposable subscription;
+
+        private readonly Extractor extractor = new Extractor();
+
+        private readonly Dictionary<string, IStockTracker> trackersTable = new Dictionary<string, IStockTracker>(StringComparer.OrdinalIgnoreCase);
+
+
+        public StreamMonitor(IAuthentication authentication, ISentimentAnalysis sentiment, IStockTracker[] trackers)
         {
             this.authentication = authentication ?? throw new ArgumentNullException(nameof(authentication));
             this.sentiment = sentiment ?? throw new ArgumentNullException(nameof(sentiment));
+            trackersTable = trackers.ToDictionary(item => item.Twitter, item => item);
+            Trackers = trackers;
         }
 
-        public void Start(string path, IStockTracker[] trackers)
+        public IStockTracker[] Trackers { get; }
+
+        public void Start(string path)
         {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(path));
+            }
+
+            path.EnsureDirectoryExistence();
             streamSource = new TimingStreamSource(path, TimeSpan.FromDays(1));
             serializer = new FlatFileSerializer(streamSource);
-            stream = new MonitoringStream(this, authentication);
-            stream.Start(trackers.Select(item => item.Stock).ToArray(), new string[]{});
+            stream = new MonitoringStream(authentication);
+            subscription = stream.MessagesReceiving
+                .ObserveOn(TaskPoolScheduler.Default)
+                .Select(Save)
+                .Merge()
+                .Subscribe(item =>
+                {
+                    log.Debug("Processed message: {0}", item.Text);
+                });
+            Task.Factory.StartNew(async () => await stream.Start(Trackers.Select(item => item.Stock).ToArray(), new string[] { }), TaskCreationOptions.LongRunning);
         }
 
         public void Dispose()
         {
+            subscription?.Dispose();
             streamSource?.Dispose();
             stream?.Dispose();
         }
 
-        public void Save(ITweetDTO tweet)
+        private async Task<ITweetDTO> Save(ITweetDTO tweet)
         {
-            serializer?.Save(tweet);
+            var saveTask = Task.Run(() => serializer?.Save(tweet));
+            var sentimentValue = await sentiment.MeasureSentiment(tweet.Text);
+            foreach (var cashTag in extractor.ExtractCashtags(tweet.Text))
+            {
+                if (trackersTable.TryGetValue(cashTag, out var tracker))
+                {
+                    tracker.AddRating(sentimentValue);
+                }
+            }
+
+            await saveTask;
+            return tweet;
         }
     }
 }
